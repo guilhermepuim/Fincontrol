7dd72146a106d3381b0b51639edb844ca8d25650
guilhermepuim <guilhermepuim@gmail.com>
2026-04-29 09:42:54 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZTu_ynlaQw70wR4KWVH4D8BTbt0gSNrk",
+  authDomain: "fincontrol-4f015.firebaseapp.com",
+  projectId: "fincontrol-4f015",
+  storageBucket: "fincontrol-4f015.firebasestorage.app",
+  messagingSenderId: "995369986592",
+  appId: "1:995369986592:web:57c9cd59cf2793bad42a45"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
